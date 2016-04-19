74a6a6d2854448c3f73f04a71d917317daa49a18
Duy <duynl_58@vnu.edu.vn>
2016-04-19 20:17:58 +0700

    Tesseract.
    1st commit. Should merge code later

---
+++ tesseract.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import pytesseract
+import glob
+import os
+
+imageList = glob.glob("out\\*.tiff")
+
+for imgPath in imageList:
+    print imgPath, pytesseract.image_to_string(Image.open(imgPath), config='-psm 6 config' )
